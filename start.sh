7b1f1d9b4177fadebab3cb8f2b38e5501c090ce3
Rohesh-Gavit <153485430+Rohesh-Gavit@users.noreply.github.com>
2023-12-14 10:40:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Rohesh-Gavit/PremiumFilter / PremiumFilter
+  git clone https://github.com/ritheshrkrm/PiroAutoFilterBot.git /PiroAutoFilterBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO / PremiumFilter
+  git clone $UPSTREAM_REPO /PiroAutoFilterBot
 fi
-cd / PremiumFilter
+cd /PiroAutoFilterBot
 pip3 install -U -r requirements.txt
 echo "Starting PiroAutoFilterBot...."
 python3 bot.py
