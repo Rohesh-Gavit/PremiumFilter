625cd742dc6065477d84dcf129e2719dafd887e0
Rohesh-Gavit <153485430+Rohesh-Gavit@users.noreply.github.com>
2023-12-25 10:14:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -36,7 +36,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Telegram")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL','-1002065343310' ))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL','-1001713712725' ))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', '')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), False)
